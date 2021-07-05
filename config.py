abf6c93d4a82fda0a2a808d1a2045516d7c35494
Sebastian <sebmora1055@gmail.com>
2021-07-05 16:24:09 -0700

    adding config file

---
+++ config.py
@@ -0,0 +1,11 @@
+neo4j_config = {
+    "host": "bolt://localhost:11003",
+    "user": "neo4j",
+    "pass": "password",
+    "db":""
+}
+
+sso_config = {
+    "region": "us-west-2",
+    "url": "https://sso_name.awsapps.com/start"
+}
