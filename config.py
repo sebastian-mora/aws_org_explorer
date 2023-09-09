0a0dfcb08ae34b99b73d5fa97ba6e0afe53673c0
seb <seb@ruse.tech>
2023-09-09 11:13:27 -0400

    remove all sso refs

--- config.py
+++ config.py
@@ -2,10 +2,5 @@ neo4j_config = {
     "host": "bolt://localhost:11003",
     "user": "neo4j",
     "pass": "password",
-    "db":""
-}
-
-sso_config = {
-    "region": "us-west-2",
-    "url": "https://sso_name.awsapps.com/start"
+    "db": ""
 }
--- mapper.py
+++ mapper.py
@@ -1,7 +1,6 @@
 import boto3
 import botocore
 
-from core.sso import retrieve_aws_sso_token, retrieve_aws_accounts, retrieve_credentials, retrieve_roles_in_account
 from core.iamEnum import retreive_roles, retreive_users
 from core.db import Db
 
@@ -10,73 +9,32 @@ import concurrent.futures
 from config import neo4j_config, sso_config
 
 
-def get_token_from_cache():
+def process_account(assumed_role_arn, db):
+    print(f"\tListing {account['accountId']} using role, {assumed_role_arn}")
+
+    iamClient = boto3.client("iam")
     try:
-        with open("token", 'r') as f:
-            return f.readline()
-    except FileNotFoundError:
-        return None
+        roles = retreive_roles(iamClient)
+        users = retreive_users(iamClient)
 
+        for role in roles:
+            db.add_aws_role(role)
+        for user in users:
+            db.add_aws_user(user)
 
-def save_token_to_cache(token):
-    with open("token", 'w') as f:
-        f.write(token)
-
-
-def process_account(sso, aws_sso_token, account, db):
-
-    sso_roles = retrieve_roles_in_account(sso, aws_sso_token, account)
-
-    # Loop through roles. If role get permission error on list, try next.
-    for access_role in sso_roles:
-
-        print(f"\tListing {account['accountId']} using role, {access_role}")
-
-        aws_access_key_id, aws_secret_access_key, aws_session_token = retrieve_credentials(
-            sso, aws_sso_token, account['accountId'], access_role)
-
-        iamClient = boto3.client('iam', aws_access_key_id=aws_access_key_id,
-                                 aws_secret_access_key=aws_secret_access_key,
-                                 aws_session_token=aws_session_token)
-        try:
-            roles = retreive_roles(iamClient)
-            users = retreive_users(iamClient)
-            
-            for role in roles:
-                db.add_aws_role(role)
-            for user in users:
-                db.add_aws_user(user)
-
-            # If no exceptions were had break this loop and start next account
-            break
-
-        except botocore.exceptions.ClientError as error:
-            if error.response['Error']['Code'] == 'AccessDenied':
-                print(
-                    f"\tRole {access_role} does not have permissions to list users/roles...trying next role")
-                continue
+    except botocore.exceptions.ClientError as error:
+        if error.response["Error"]["Code"] == "AccessDenied":
+            print(
+                f"\tRole {assumed_role_arn} does not have permissions to list users/roles...trying next role"
+            )
 
 
 if __name__ == "__main__":
-    aws_sso_token = get_token_from_cache()
-    db = Db(neo4j_config['host'], neo4j_config['user'], neo4j_config['pass'])
-    sso = boto3.client('sso', region_name=sso_config['region'])
+    db = Db(neo4j_config["host"], neo4j_config["user"], neo4j_config["pass"])
 
-    try:
-        aws_accounts_list = retrieve_aws_accounts(sso, aws_sso_token)
-    except Exception as error:
-        aws_sso_token = retrieve_aws_sso_token(None)
-        save_token_to_cache(aws_sso_token)
-        aws_accounts_list = retrieve_aws_accounts(sso, aws_sso_token)
+    account = ""
+    role = ""
 
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        futures = []
-        count = 0
-        for account in aws_accounts_list:
-            db.add_aws_account(account)
-            futures.append(executor.submit(
-                process_account, sso, aws_sso_token, account, db))
+    db.add_aws_account(account)
 
-        for future in concurrent.futures.as_completed(futures):
-            count += 1
-            print(f"Completed ({count}/{len(aws_accounts_list)})")
+    process_account(role, db)
