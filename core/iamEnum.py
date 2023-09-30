eb5a30dd4f2cc05f67b5d42bb0ff04b30977bab8
seb <seb@ruse.tech>
2023-09-30 11:20:50 -0400

    removed sso

--- core/iamEnum.py
+++ core/iamEnum.py
@@ -1,24 +1,20 @@
-
-def retreive_roles(client):
-
+def get_roles(client):
     res = client.list_roles()
-    roles = res['Roles']
+    roles = res["Roles"]
 
     while res.get("Marker"):
         res = client.list_roles(Marker=res.get("Marker"))
-        roles.extend(res['Roles'])
+        roles.extend(res["Roles"])
 
     return roles
 
-def retreive_users(client):
 
+def get_users(client):
     res = client.list_users()
-    users = res['Users']
+    users = res["Users"]
 
     while res.get("Marker"):
         res = client.list_roles(Marker=res.get("Marker"))
-        users.extend(res['Users'])
+        users.extend(res["Users"])
 
     return users
-
-
--- mapper.py
+++ mapper.py
@@ -1,11 +1,10 @@
 import boto3
 import botocore
 
-from core.iamEnum import retreive_roles, retreive_users
+from core.iamEnum import get_roles, get_users
 from core.db import Db
+from boto3 import session
 
-
-from config import neo4j_config
 
 import os
 from dotenv import load_dotenv
@@ -13,13 +12,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def process_account(assumed_role_arn, db):
-    print(f"\tListing {account['accountId']} using role, {assumed_role_arn}")
+def process_account(iamClient, db):
+    # print(f"\tListing {account['accountId']} using role, {assumed_role_arn}")
 
-    iamClient = boto3.client("iam")
     try:
-        roles = retreive_roles(iamClient)
-        users = retreive_users(iamClient)
+        roles = get_roles(iamClient)
+        users = get_users(iamClient)
 
         for role in roles:
             db.add_aws_role(role)
@@ -27,10 +25,24 @@ def process_account(assumed_role_arn, db):
             db.add_aws_user(user)
 
     except botocore.exceptions.ClientError as error:
-        if error.response["Error"]["Code"] == "AccessDenied":
-            print(
-                f"\tRole {assumed_role_arn} does not have permissions to list users/roles...trying next role"
-            )
+        return error
+
+
+def assume_aws_role(role_arn, session_name, duration_seconds=3600) -> session:
+    sts_client = boto3.client("sts")
+    response = sts_client.assume_role(
+        RoleArn=role_arn, RoleSessionName=session_name, DurationSeconds=duration_seconds
+    )
+
+    # Create a new Boto3 session using the temporary credentials
+    temporary_credentials = response["Credentials"]
+    session = boto3.Session(
+        aws_access_key_id=temporary_credentials["AccessKeyId"],
+        aws_secret_access_key=temporary_credentials["SecretAccessKey"],
+        aws_session_token=temporary_credentials["SessionToken"],
+    )
+
+    return session
 
 
 if __name__ == "__main__":
@@ -40,11 +52,9 @@ if __name__ == "__main__":
         user=os.getenv("NEO4J_USER"),
     )
 
-    print(db.ping())
+    # Assume role in account
+    assume_role_arn = ""
+    session = assume_aws_role(assume_aws_role)
+    iamClient = session.client("ec2")
 
-    # account = ""
-    # role = ""
-
-    # db.add_aws_account(account)
-
-    # process_account(role, db)
+    process_account(iamClient, db)
